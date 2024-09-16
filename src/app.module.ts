a072934d42fc9e8421144e46a1545ec0a7b671c7
thetinshusasi <thetinshusasi@gmail.com>
2024-09-16 09:53:50 -0300

    feat(app): Ticket 1 Added Postgres
    Added Docker support  for Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { LinkInBioModule } from './link-in-bio/link-in-bio.module';
 
@@ -6,6 +7,18 @@ import { LinkInBioModule } from './link-in-bio/link-in-bio.module';
 @Module({
 
 
-  imports: [UserModule, LinkInBioModule]
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: +process.env.DB_PORT || 5432,
+      username: process.env.DB_USERNAME || 'nest_user',
+      password: process.env.DB_PASSWORD || 'nest_password',
+      database: process.env.DB_NAME || 'nest_db',
+      autoLoadEntities: true,
+      synchronize: true, // Disable in production
+    }),
+    UserModule,
+    LinkInBioModule]
 })
 export class AppModule { }
