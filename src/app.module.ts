68597f9b2e5e4110cd1cf969f45e772096bce3a2
thetinshusasi <thetinshusasi@gmail.com>
2024-09-16 11:05:05 -0300

    feat(app): Ticket 1 Added Logger Middileware
    Added Pino Logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerModule } from 'nestjs-pino';
+
 import { UserModule } from './user/user.module';
 import { LinkInBioModule } from './link-in-bio/link-in-bio.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 
 @Module({
 
 
   imports: [
+    LoggerModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST || 'localhost',
-      port: +process.env.DB_PORT || 5432,
+      port: +process.env.DB_PORT || 5433,
       username: process.env.DB_USERNAME || 'nest_user',
       password: process.env.DB_PASSWORD || 'nest_password',
       database: process.env.DB_NAME || 'nest_db',
@@ -19,6 +23,10 @@ import { LinkInBioModule } from './link-in-bio/link-in-bio.module';
       synchronize: true, // Disable in production
     }),
     UserModule,
-    LinkInBioModule]
+    LinkInBioModule],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(Logger));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    })
+  )
   await app.listen(3000);
 }
 bootstrap();
